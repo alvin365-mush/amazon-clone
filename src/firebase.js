8b492b29b1577bb8c2918d572fb4bb19d2b69c1f
al <alvinmusiega@gmail.com>
2021-05-19 14:49:55 +0300

    my second commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA9pgAirf3qhlpTSPjSYyAsbIqKUljHgzM",
-  authDomain: "clone-22134.firebaseapp.com",
-  projectId: "clone-22134",
-  storageBucket: "clone-22134.appspot.com",
-  messagingSenderId: "583012749095",
-  appId: "1:583012749095:web:56076742328275c044e302",
-  measurementId: "G-ZZX8EVXTGQ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
